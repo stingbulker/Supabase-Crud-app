2775cc881aa2343714c79bf9c0c69a0f75677097
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-05-29 17:37:33 +0530

    editing state

--- src/productCard.js
+++ src/productCard.js
@@ -1,17 +1,19 @@
-import {Card,Button,Form} from 'react-bootstrap'
+import { useState } from "react";
+import { Card, Button, Form } from "react-bootstrap";
 
 function ProductCard() {
+  const [editing, setEditing] = useState(false);
 
-  return(
-    <Card style={{width: "18rem"}}>
+  return (
+    <Card style={{ width: "18rem" }}>
       <Card.Body>
         <Card.Title>Product Name</Card.Title>
         <Card.Text>Product Description</Card.Text>
-        <Button variant='danger'>Delete Product</Button>
-        <Button variant='secondary'>Edit Product</Button>
+        <Button variant="danger">Delete Product</Button>
+        <Button variant="secondary">Edit Product</Button>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
 export default ProductCard;
